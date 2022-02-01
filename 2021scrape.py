72d876067b8a476400b26c1a09c669064e8df941
henryhart116 <74627459+henryhart116@users.noreply.github.com>
2022-02-01 16:12:21 -0500

    2021 game result data

---
+++ 2021scrape.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+
+#Importing data
+data = pd.read_csv('cbb.csv', header=0)
+teams = data['TEAM'].unique()
+print(teams)
+url1 = 'https://barttorvik.com/team.php?team='
+url2 = '&year=2021'
+print(len(teams))
+# Scraping function
+url = url1+teams[0].replace(' ','+')+url2
+page = requests.get(url)
+schedule_table = BeautifulSoup(page.content, 'html.parser').find("table", attrs={"class": "skedtable"}).tbody
+schedule_table = schedule_table.find_all("tr")
+schedule_table = schedule_table[:-1]
+print(schedule_table[0].find_all("td")[7].a.text)
+#matchups = []
+#for i in schedule_table:
+#    matchups.append([teams[0],i.find_all("td")[5].a.text,i.find_all("td")[7].a.text[0]])
+
+# Looping through teams
+matchup_data = []
+for i in teams:
+    try:
+        i = i.replace(' ','+')
+        url = url1+i+url2
+        page = requests.get(url)
+        schedule_table = BeautifulSoup(page.content, 'html.parser').find("table", attrs={"class": "skedtable"}).tbody
+        schedule_table = schedule_table.find_all("tr")
+        schedule_table = schedule_table[:-1]
+        #print(schedule_table[0].find("td", attrs={"class": "mobileout"}).a.text)
+        for j in schedule_table:
+            try:
+                matchup_data.append([i,j.find_all("td")[5].a.text,j.find_all("td")[5].a.text,j.find_all("td")[7].a.text])
+            except:
+                pass
+    except:
+        print(i)
+        pass
+matchups = pd.DataFrame(matchup_data, columns=['date','team','opponent','result'])
+matchups.to_csv('2021Matchups.csv',index=False)
