3aceea0a550c6dc88ba2b4208f630efb61cb1ce5
henryhart116 <74627459+henryhart116@users.noreply.github.com>
2022-02-01 17:24:14 -0500

    updates

--- 2021scrape.py
+++ 2021scrape.py
@@ -6,17 +6,15 @@ from bs4 import BeautifulSoup
 #Importing data
 data = pd.read_csv('cbb.csv', header=0)
 teams = data['TEAM'].unique()
-print(teams)
 url1 = 'https://barttorvik.com/team.php?team='
 url2 = '&year=2021'
-print(len(teams))
 # Scraping function
 url = url1+teams[0].replace(' ','+')+url2
 page = requests.get(url)
 schedule_table = BeautifulSoup(page.content, 'html.parser').find("table", attrs={"class": "skedtable"}).tbody
 schedule_table = schedule_table.find_all("tr")
 schedule_table = schedule_table[:-1]
-print(schedule_table[0].find_all("td")[7].a.text)
+print(schedule_table[0].find_all("td")[0].text.split('\n')[2])
 #matchups = []
 #for i in schedule_table:
 #    matchups.append([teams[0],i.find_all("td")[5].a.text,i.find_all("td")[7].a.text[0]])
@@ -34,7 +32,7 @@ for i in teams:
         #print(schedule_table[0].find("td", attrs={"class": "mobileout"}).a.text)
         for j in schedule_table:
             try:
-                matchup_data.append([i,j.find_all("td")[5].a.text,j.find_all("td")[5].a.text,j.find_all("td")[7].a.text])
+                matchup_data.append([j.find_all("td")[0].text.split('\n')[2],i,j.find_all("td")[5].a.text,j.find_all("td")[7].a.text])
             except:
                 pass
     except:
